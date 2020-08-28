5cffde78bf6cd60c054c56792ec8b2b8bc064b05
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-08-28 12:41:53 -0400

    solving all problems

---
+++ patrones.rb
@@ -0,0 +1,234 @@
+# a. Método letra_o(n)
+# n = 5
+# *****
+# *   *
+# *   *
+# *   *
+# *****
+print "\nfirst exercise \n\n"
+
+
+n = ARGV[0].to_i
+
+n.times do |i|
+    (i - 3).times do |j|
+        print "*****"
+    end
+end
+print "\n"
+(n - 2).times do |i|
+    (n / 5).times do |j|
+        print "*   *"
+    end
+    print "\n"
+end
+
+n.times do |i|
+    (i - 3).times do |j|
+        print "*****"
+    end
+end
+print "\n"
+
+
+
+print "\nsecond exercise: \n\n"
+# b. Método letra_i(n)
+# n = 5
+# *****
+#   *
+#   *
+#   *
+# *****
+
+n = ARGV[0].to_i
+
+n.times do |i|
+    (i - 3).times do |j|
+        print "*****"
+    end
+end
+print "\n"
+(n - 2).times do |i|
+    (n / 5).times do |j|
+        print "  *   "
+    end
+    print "\n"
+end
+
+n.times do |i|
+    (i - 3).times do |j|
+        print "*****"
+    end
+end
+print "\n"
+
+
+
+
+print "\nthird exercise: \n\n"
+
+# c. Método letra_z(n)
+# n = 5
+# *****
+#    *
+#   *
+#  *
+# *****
+
+n = ARGV[0].to_i
+
+n.times do |i|
+    (i - 3).times do |j|
+        print "*****"
+    end
+end
+(n / 5).times do |i|
+    (n / 5).times do |j|
+        print "    
+   *
+  *
+ *"
+    end
+    print "\n"
+end
+
+n.times do |i|
+    (i - 3).times do |j|
+        print "*****"
+    end
+end
+print "\n"
+
+
+print "\nfourth exercise: \n\n"
+
+# d. Método letra_x(n)
+# n = 5
+# *   *
+#  * *
+#   *
+#  * *
+# *   *
+
+n = ARGV[0].to_i
+
+n.times do |i|
+    (i - 3).times do |j|
+        print "*   *"
+    end
+end
+(n / 5).times do |i|
+    (n / 5).times do |j|
+        print "
+ * *
+  *"
+    end
+end
+(n - 4).times do |i|
+    (n - 4).times do |j|
+        print "
+ * *"
+    end
+    print "\n"
+end
+n.times do |i|
+    (i - 3).times do |j|
+        print "*   *"
+    end
+end
+print "\n"
+
+
+
+
+print "\nfifth exercise: \n\n"
+
+
+# e. Método numero_cero(n)
+# n = 5
+# *****
+# **  *
+# * * *
+# *  **
+# *****
+
+n = ARGV[0].to_i
+
+n.times do |i|
+    (i - 3).times do |j|
+        print "*****"
+    end
+end
+print "\n"
+
+(n - 4).times do |i|
+    (n - 4).times do |j|
+        print "**  *"
+    end
+    print "\n"
+end
+(n - 4).times do |i|
+    (n - 4).times do |j|
+        print "* * *"
+    end
+    print "\n"
+end
+(n - 4).times do |i|
+    (n - 4).times do |j|
+        print "*  **"
+    end
+    print "\n"
+end
+n.times do |i|
+    (i - 3).times do |j|
+        print "*****"
+    end
+end
+print "\n"
+
+print "\nsixth exercise: \n\n"
+
+
+# f. Método navidad(n)
+# n = 5
+#    *
+#   * *
+#  * * *
+# * * * *
+#    *
+#    *
+#  * * *
+
+(n / 5).times do |i|
+    (n / 5).times do |j|
+        print "
+   *
+  * *"
+    end
+    print "\n"
+end
+n.times do |i|
+    (i - 3).times do |j|
+        print " * * *"
+    end
+end
+print "\n"
+n.times do |i|
+    (i - 3).times do |j|
+        print "* * * *"
+    end
+end
+print "\n"
+(n - 3).times do |i|
+    (n / 5).times do |j|
+        print "   *  "
+    end
+    print "\n"
+end
+n.times do |i|
+    (i - 3).times do |j|
+        print " * * *"
+    end
+end
+print "\n"
